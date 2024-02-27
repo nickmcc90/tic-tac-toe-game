a9961ef6d149fdafd6c143c7a5f733668d0e850c
Nick <133172211+nickmcc90@users.noreply.github.com>
2024-02-26 19:18:01 -0500

    2/26

---
+++ scripts/beginning.js
@@ -0,0 +1,30 @@
+document.querySelector('.js-play-game').addEventListener('click', () => {
+  askWhoIsFirst();
+})
+
+function askWhoIsFirst() {
+  const gameSpace = document.querySelector('.js-play-game-container');
+  gameSpace.innerHTML = ''; // button disappears
+
+  const question = `
+  <div class='question'>Who shall go first in this Tic Tac Toe game?</div>
+  <div class="decision-space js-decision-space">
+    <button class='js-human human-button'>Me</button>
+    <button class='js-computer computer-button'>Computer</button>
+  </div>
+  `;
+
+  document.querySelector('.js-question-container').innerHTML = question;    // gets replaced with questions and decisions
+
+  document.querySelector('.js-human').addEventListener('click', () => {
+    displaySquares();
+  })
+
+  document.querySelector('.js-computer').addEventListener('click', () => {
+    computerMove();
+  })
+}
+
+function questionContainerDelete() {
+  document.querySelector('.js-question-container').innerHTML = '';
+}
---
+++ scripts/code.js
@@ -0,0 +1,79 @@
+const classes = ['.js-one', '.js-two', '.js-three', '.js-four', '.js-five', '.js-six', '.js-seven', '.js-eight', '.js-nine'];
+
+classes.forEach((value, index) => {
+  document.querySelector(value)
+})
+
+
+
+// above is the square event listeners....^^^
+
+let timerId;
+const determinedChoice = Math.random();
+function computerMove() {
+  questionContainerDelete();
+  let space;
+  let choice;
+  const random = Math.random();
+  if(random < 0.1) {
+    space = '.js-one';
+  } else if (random > 0.1 && random < 0.2) {
+    space = '.js-two';
+  } else if (random > 0.2 && random < 0.3) {
+    space = '.js-three';
+  } else if (random > 0.3 && random < 0.4) {
+    space = '.js-four';
+  } else if (random > 0.4 && random < 0.5) {
+    space = '.js-five'; 
+  } else if (random > 0.5 && random < 0.6) {
+    space = '.js-six';
+  } else if (random > 0.6 && random < 0.7) {
+    space = '.js-seven';
+  } else if (random > 0.7 && random < 0.8) {
+    space = '.js-eight';
+  } else if (random > 0.8 && random < 0.9) {
+    space = '.js-nine';
+  } else {
+    space = '.js-five';
+  };
+
+  if(determinedChoice < 0.5) {
+    choice = 'x';
+  } else {
+    choice = 'o';
+  }
+
+  computerThinking();
+
+  timerId = setTimeout(() => {
+    displaySquares(space, choice);
+  }, 4000);
+}
+
+function displaySquares(output, choice) {
+  document.querySelector(output).innerHTML = choice;
+}
+
+function computerThinking() {
+  const thinkingElement = document.querySelector('.js-computer-thinking');
+  thinkingElement.innerHTML = 'Computer thinking....'
+  let value = 3;
+  let intId;
+  intId = setInterval(() => {
+    document.querySelector('.js-timer').innerHTML = value;
+    value--;
+  },1000);
+  setTimeout(() => {
+    clearInterval(intId)
+    thinkingElement.innerHTML = '';
+    document.querySelector('.js-timer').innerHTML = '';
+    playerSelectText(); 
+  },4000);
+}
+
+function playerSelectText() {     // trying a new format of adding html with js
+  const playerText = document.createElement("div");
+  playerText.textContent = "Choose a square..... try to beat the computer!";
+  playerText.className = 'player-text';
+  document.querySelector('.js-player-text-container').append(playerText);
+}
